06debf9d59cbae5d930bace4060aaf1085d5d717
Eyver Vergara <eveloxd@gmail.com>
2025-12-30 01:43:59 -0500

    cambios sede recreo y refresh home

--- app/page.tsx
+++ app/page.tsx
@@ -28,22 +28,9 @@ const Home: React.FC<HomeProps> = ({
   useEffect(() => {
     fetch(`${API_BASE_URL}/api/sedes`)
       .then((res) => res.json())
-      // .then((data) => {
-      //   setSedes(Array.isArray(data) ? data : []);
-      // })
       .then((data) => {
-        if (!Array.isArray(data)) {
-          setSedes([]);
-          return;
-        }
-
-        const filtradas = data.filter((sede: Sede) =>
-          sede.nombre.toLowerCase().includes("alamedas")
-        );
-
-        setSedes(filtradas);
+        setSedes(Array.isArray(data) ? data : []);
       })
-
       .catch(() => setSedes([]))
       .finally(() => setLoading(false));
   }, [API_BASE_URL]);
@@ -57,7 +44,7 @@ const Home: React.FC<HomeProps> = ({
     <div className="flex flex-col min-h-screen bg-black text-white justify-center items-center p-16 sm:p-10 lg:p-16">
       <div className="flex flex-col items-center justify-center w-full max-w-lg mx-auto">
         <h1
-          className="text-5xl sm:text-7xl lg:text-8xl font-black leading-tight uppercase tracking-wider mb-16 lg:mb-20 text-center"
+          className="text-5xl sm:text-5xl lg:text-6xl font-black leading-tight uppercase tracking-wider mb-16 lg:mb-20 text-center"
           style={{
             fontFamily:
               "'Avenir Black', 'Avenir-Heavy', 'Avenir', sans-serif",
@@ -67,6 +54,7 @@ const Home: React.FC<HomeProps> = ({
           <br />
           TU CITA
         </h1>
+
         <div className="flex flex-col space-y-4 w-full max-w-sm md:max-w-md">
           {loading && (
             <div className="text-center text-sm text-gray-400">
@@ -85,21 +73,22 @@ const Home: React.FC<HomeProps> = ({
               </button>
             ))}
 
-
-          {/* WhatsApp (puedes luego hacerlo por sede si quieres) */}
+          {/* WhatsApp Alamedas */}
           <a
             href={whatsappLink}
             target="_blank"
             rel="noopener noreferrer"
-            className="w-full flex items-center justify-center border border-white bg-black rounded-[18px] py-3 px-6 text-sm md:text-base lg:text-lg font-medium uppercase tracking-widest transition-colors duration-300 hover:bg-white hover:text-black"
+            className="w-full flex text-center items-center justify-center border border-white bg-black rounded-[18px] py-3 px-6 text-sm md:text-base lg:text-lg font-medium uppercase tracking-widest transition-colors duration-300 hover:bg-white hover:text-black"
           >
             WhatsApp Sede Alamedas
           </a>
-                    <a
+
+          {/* WhatsApp Recreo */}
+          <a
             href={whatsappLinkRecreo}
             target="_blank"
             rel="noopener noreferrer"
-            className="w-full flex items-center justify-center border border-white bg-black rounded-[18px] py-3 px-6 text-sm md:text-base lg:text-lg font-medium uppercase tracking-widest transition-colors duration-300 hover:bg-white hover:text-black"
+            className="w-full flex text-center items-center justify-center border border-white bg-black rounded-[18px] py-3 px-6 text-sm md:text-base lg:text-lg font-medium uppercase tracking-widest transition-colors duration-300 hover:bg-white hover:text-black"
           >
             WhatsApp Sede Recreo
           </a>
